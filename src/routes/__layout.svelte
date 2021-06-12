7572992f91aab20b586af410d86e34ecd4700b42
Philip Krogel <pkrogel@krogelwebsolutions.com>
2021-06-12 11:43:31 -0400

    Initial Commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import 'virtual:windi.css';
+	import Header from '$lib/Header/index.svelte';
+	import '../app.css';
+
+</script>
+
+<Header />
+
+<main flex="~ 1 col" w="full" max-w="1024px" m="x-auto y-0" p="4" box="border">
+	<slot />
+</main>
+
+<footer flex="~ col" justify="center" items="center" p="4 sm:(y-8 x-0)" font="bold">
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
---
+++ windi.config.ts
@@ -0,0 +1,8 @@
+// windi.config.ts
+export default {
+	attributify: true,
+	shortcuts: {
+		hstack: 'flex items-center',
+		vstack: 'flex flex-col'
+	}
+};
